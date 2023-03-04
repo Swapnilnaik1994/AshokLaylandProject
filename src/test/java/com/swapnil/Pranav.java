2fc5e033e1c0d82854dd470f5108e53586ce65cc
swapnil naik <swapnilnaik1994@gmail.com>
2023-03-05 00:01:14 +0530

    Basu friend is add

--- src/test/java/com/swapnil/Pranav.java
+++ src/test/java/com/swapnil/Pranav.java
@@ -5,7 +5,8 @@ public class Pranav {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Swapnil and Pranav are Best friend");
+		System.out.println("Swapnil and Pranav are Best friends");
+		System.out.println("Swapnil and Basu are Best friends");
 	}
 
 }
